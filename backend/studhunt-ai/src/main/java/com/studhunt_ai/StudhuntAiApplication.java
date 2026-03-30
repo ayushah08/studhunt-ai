38580b95a37a27a469245bc116272751beaab744
ayushah08 <ayushah810@gmail.com>
2026-03-30 22:36:49 +0530

    Initial Backend project startup

---
+++ backend/studhunt-ai/src/main/java/com/studhunt_ai/StudhuntAiApplication.java
@@ -0,0 +1,13 @@
+package com.studhunt_ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudhuntAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudhuntAiApplication.class, args);
+	}
+
+}
