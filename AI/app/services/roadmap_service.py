a2658bebf3e62fa4e651d94ca9c93194329bc0ad
Divesh455 <matkardivesh26@gmail.com>
2026-03-31 12:10:21 +0530

    change_1

--- AI/app/services/roadmap_service.py
+++ AI/app/services/roadmap_service.py
@@ -1,14 +1,14 @@
-from langchain.chains import LLMChain
 from app.core.llm import get_llm
 from app.utils.prompt import get_prompt
 
 llm = get_llm()
 prompt = get_prompt()
 
-chain = LLMChain(llm=llm, prompt=prompt)
+# NEW PIPELINE
+chain = prompt | llm
 
 def generate_roadmap(goal, level, language):
-    response = chain.run({
+    response = chain.invoke({
         "goal": goal,
         "level": level,
         "language": language
